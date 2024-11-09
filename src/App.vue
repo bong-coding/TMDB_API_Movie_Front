e7e80f802880869e0817eda8fbde1caed31240dd
bong-coding <qhdals1611@naver.com>
2024-11-10 01:11:49 +0900

    footer

--- src/App.vue
+++ src/App.vue
@@ -6,36 +6,18 @@
     <router-view />
     <Footer />
     <!-- Footer 컴포넌트 포함 -->
-
-    <!-- MovieCard를 App.vue에서 직접 사용 (테스트용) -->
-    <div class="test-movie-card">
-      <MovieCard :movie="testMovie" />
-    </div>
   </div>
 </template>
 
 <script>
 import Header from "./components/common/Header.vue";
 import Footer from "./components/common/Footer.vue";
-import MovieCard from "./components/common/MovieCard.vue";
 
 export default {
   name: "App",
   components: {
     Header,
     Footer,
-    MovieCard,
-  },
-  data() {
-    return {
-      testMovie: {
-        id: 1,
-        title: "Test Movie",
-        overview:
-          "This is a test movie overview to demonstrate the MovieCard component.",
-        poster_path: "/testposter.jpg",
-      },
-    };
   },
 };
 </script>
@@ -49,7 +31,5 @@ export default {
   color: #2c3e50;
 }
 
-.test-movie-card {
-  margin: 20px;
-}
+/* 기존에 있던 test-movie-card 스타일은 제거 */
 </style>
