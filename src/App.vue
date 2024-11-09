f31707075fc4f9e9d7b99192551d36782ef8ab3e
bong-coding <qhdals1611@naver.com>
2024-11-10 00:33:18 +0900

    main_update

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,44 @@
+<!-- src/App.vue -->
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div id="app">
+    <Header />
+    <!-- Header 컴포넌트 포함 -->
+    <router-view />
+    <Footer />
+    <!-- Footer 컴포넌트 포함 -->
+
+    <!-- MovieCard를 App.vue에서 직접 사용 (테스트용) -->
+    <div class="test-movie-card">
+      <MovieCard :movie="testMovie" />
+    </div>
+  </div>
 </template>
+
+<script>
+import Header from "./components/common/Header.vue";
+import Footer from "./components/common/Footer.vue";
+import MovieCard from "./components/common/MovieCard.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Footer,
+    MovieCard,
+  },
+  data() {
+    return {
+      testMovie: {
+        id: 1,
+        title: "Test Movie",
+        overview:
+          "This is a test movie overview to demonstrate the MovieCard component.",
+        poster_path: "/testposter.jpg",
+      },
+    };
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -15,16 +49,7 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.test-movie-card {
+  margin: 20px;
 }
 </style>
